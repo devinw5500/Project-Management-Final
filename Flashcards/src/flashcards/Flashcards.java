a9e3de8e7ac553fd26d0ba5686def203ef5ca4ba
devinw5500 <43462307+devinw5500@users.noreply.github.com>
2019-03-25 08:49:34 -0400

    Added the start of the java program

---
+++ Flashcards/src/flashcards/Flashcards.java
@@ -0,0 +1,22 @@
+/*
+ * Make a GUI in Java that displays flashcards (like quizlet)
+ * Read and Write to a text file that holds the "cards"
+ * Display the cards in a JFrame 
+ * Be able to switch between cards and flip cards
+ * Devin Wiggins, Jason Tressler, Jacob Young
+ */
+package flashcards;
+
+//import statements
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Flashcards 
+{
+    public static void main(String[] args) 
+    {
+        //comment thing
+        
+    }
+}
